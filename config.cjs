32bee01fcba3d12991c763c7852a6d58781f3b45
Madawaki1 <136708942+Madawaki1@users.noreply.github.com>
2024-10-27 16:19:29 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&Bf6CgkFj",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&TkZdSpq2",
   PREFIX: process.env.PREFIX || ',',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
